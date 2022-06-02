60492dc86687f4b789ce956157fc69ec050022ca
zhakos <93442541+zhakos@users.noreply.github.com>
2022-06-02 22:25:10 +0500

    its me 2 but right version

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ conn = sqlite3.connect('databases/subjects.db')
 cur = conn.cursor()
 
 # Объект бота
-bot = Bot(token="5332706447:AAH-GxB4NtiKUnW-6vKOBWVwfzBqhnPhctU")
+bot = Bot(token="5322427961:AAElTOBaFWlfxonWpzYRIO7TZK-JhtNuU0s")
 # Диспетчер для бота
 dp = Dispatcher(bot)
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -66,7 +66,7 @@ async def cmd_chem(message: types.Message):
 
 
 # ////////////////////////Geography/////////////////////
-@dp.message_handler(lambda message: 'гео' in message.text.lower())
+@dp.message_handler(lambda message: 'гео' in message.text.lower() and '-' not in message.text.lower())
 async def cmd_geo(message: types.Message):
     buttons = [
         types.InlineKeyboardButton(text="Биология-География", callback_data='Биология-География'),
@@ -128,11 +128,13 @@ async def cmd_back(message: types.Message):
     await message.answer("Здравствуйте", reply_markup=keyboard)
 
 
-def request(message):
-    cur.execute(f"SELECT groups.nomer, groups.name, year_2020.grant, year_2020.jk, year_2020.ak FROM groups JOIN year_2020 ON groups.nomer = year_2020.group_nomer WHERE groups.subject = '{message}' ORDER BY groups.nomer ASC;")
+def request(message, year):
+    cur.execute(f"SELECT groups.nomer, groups.name, year_{year}.grant, year_{year}.jk, year_{year}.ak FROM groups JOIN year_{year} ON groups.nomer = year_{year}.group_nomer WHERE groups.subject = '{message}' ORDER BY groups.nomer ASC;")
     data_groups = cur.fetchall()
     cur.execute(f"SELECT spec.group_nomer, spec.name FROM spec JOIN groups ON groups.nomer = spec.group_nomer WHERE subject = '{message}' ORDER BY spec.group_nomer ASC;")
     data_spec = cur.fetchall()
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name > 'year_';")
+    tables = cur.fetchall()
     text = ''
     for group in data_groups:
         group = list(group)
@@ -142,12 +144,40 @@ def request(message):
             if spec[0] == group[0]:
                 text = text + "-" + spec[1] + "-\n"  #подумай
         text = text + f"Бакалавриатқа бөлінген грант саны:{group[2]}\nЖалпы конкурс бойынша грантқа түскен минималды балл:{group[3]}\nАуылдық квотамен грантқа түскен минималды балл:{group[4]}\n\n"
-    return text    
+    return text,tables
 
-@dp.message_handler(text="Математика-Физика")
-async def math_fiz(message: types.message, call: types.callback_query):
-    await message.answer(request(message.text))
+@dp.callback_query_handler()
+@dp.message_handler(lambda message: message.text in ["Биология-География", "География-Всемирная История", "География-Английский",
+               "Всемирная История-Человек.Общество.Право", "Казахский Язык-Казахская Литература", "Математика-Физика",
+               "Математика-География", "Русский Язык-Русская Литература", "Химия-Биология",
+               "Химия-Физика", "Английский Язык-Всемирная История", "Творческий экзамен"])
+async def from_bd_balls(subject: types.message, year = None):
+    if not year:
+        year = int(str(subject.date)[:4]) - 1
+    data = request(subject.text, year)
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    buttons = []
+    for year in data[1]:
+        buttons.append(types.InlineKeyboardButton(text=f"{str(year[0])[5:]} год", callback_data=subject.text + ' ' + year[0]))
+    keyboard.add(*buttons)
+    await subject.answer(data[0],reply_markup=keyboard)
+
+@dp.callback_query_handler(Text(contains='year_'))
+async def year(call: types.CallbackQuery):
+    data = call.data.split(' ')
+    data[1] = int(str(data[1])[5:])
+    await from_bd_balls(data[0],data[1])
 
 if __name__ == "__main__":
     # Запуск бота
     executor.start_polling(dp, skip_updates=True)
+
+
+
+
+
+
+
+
+
+#{"id":"4680207043138230388","from":{"id":1089695618,"is_bot":false,"first_name":"Zhako","language_code":"en"},"message":{"message_id":307,"from":{"id":5322427961,"is_bot":true,"first_name":"Idk","username":"Korkyt_assintant_bot"},"chat":{"id":1089695618,"first_name":"Zhako","type":"private"},"date":1654155682,"text":"tyu","reply_markup":{"inline_keyboard":[[{"text":"2020 год","callback_data":"hi"}],[{"text":"2021 год","callback_data":"hi"}]]}},"chat_instance":"-8721534746082330406","data":"hi"}
